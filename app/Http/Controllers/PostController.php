40d46d2af62031f46ef0cefb0fa7501a14c4b597
waritsara Sutham <11.waritsara@gmail.com>
2020-08-16 16:52:18 +0700

    เสร็จแล้วจ้า

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::where('user_id', Auth::user()->id)->paginate(5);
         $data = [
             'posts' => $posts
         ];
@@ -24,17 +26,38 @@ class PostController extends Controller
 
     public function create()
     {
+        $categories = Category::all();
+        $data = [
+            'categories' => $categories
+        ];
+        return view('post.create', $data);
     }
-    public function store()
+
+    public function store(Request $request)
+    {
+        $detail = $request->input('detail');
+        $category_id = $request->input('category_id');
+
+        $post = new Post();
+        $post->detail = $detail;
+        $post->category_id = $category_id;
+        $post->user_id = auth()->user()->id;
+        $post->complete = 0;
+        $post->save();
+
+        return redirect('/post');
+    }
+
+    public function edit($id)
     {
     }
-    public function edit()
+
+    public function update(Request $request)
     {
     }
-    public function update()
-    {
-    }
-    public function delete()
+
+    public function delete($id)
     {
     }
 }
+
